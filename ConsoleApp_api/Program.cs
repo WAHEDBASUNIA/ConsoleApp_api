59aa4c7eff7e3ced2e4c4b178a6a299c7e11754f
WAHEDBASUNIA <wahed.online@gmail.com>
2024-10-28 15:46:55 +0600

    Add project files.

---
+++ ConsoleApp_api/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Diagnostics;
+using System.Security.Cryptography;
+//using System.Web.HttpContext;
+//using System.Threading.Tasks;
+using System.Net.Http;
+//using System.Web.Script.Serialization;
+//using Nop.Core.Domain.Payments;
+//using Nop.Plugin.Payments.ShurjoPay.Models;
+//using Nop.Services.Configuration;
+//using Nop.Services.Orders;
+using System.Xml;
+//using Nop.Services.Payments;
+//using Nop.Web.Framework.Controllers;
+using System.Globalization;
+using System.Xml.Linq;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+//using Newtonsoft.Json;
+
+namespace ConsoleApp_api
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
